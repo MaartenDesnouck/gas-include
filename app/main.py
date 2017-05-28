31805792831c033af5c5824ab4124f7860a4938d
Maarten Desnouck <maarten.desnouck@gmail.com>
2017-05-28 11:45:24 +0200

    testing stuff

--- app/main.py
+++ app/main.py
@@ -26,11 +26,11 @@ def airportName():
     """Given an airport IATA code, return that airport's name."""
     iata_code = request.args.get('iataCode')
     if iata_code is None:
-      return 'No IATA code provided.', 400
+        return 'No IATA code provided.', 400
     maybe_name = airport_util.get_airport_by_iata(iata_code)
     if maybe_name is None:
-      return 'IATA code not found : %s' % iata_code, 400
-    return maybe_name, 200
+        return 'IATA code not found : %s' % iata_code, 400
+    return 'Success:' + maybe_name, 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
